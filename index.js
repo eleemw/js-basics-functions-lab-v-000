0303572b9b716d790ce7a0df4242de811e870842
Erica Murdock-Waters <ericahpa@gmail.com>
2019-08-28 03:08:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function distanceFromHqInBlocks(pickupLocation) {
   if (pickupLocation > 42) {
     return pickupLocation - 42;
-  } else {
-      return 42 - pickupLocation;
+  }
+  else {
+    return 42 - pickupLocation;
   }
 }
